e64fe35a9f7b42bf289f2bdb3f85183b49a740d3
Serhii Ivanyk <youdex06@gmail.com>
2021-05-22 16:32:54 +0300

    Updates

---
+++ precache-manifest.e924d57ff88e358b81e0ea2765af73ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424fee460a50e812f02e2fe80548cd8e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de38f0ff793bd2661133",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "1710a128acc5825760ee",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "de38f0ff793bd2661133",
+    "url": "./static/js/2.854f3d7a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.854f3d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1710a128acc5825760ee",
+    "url": "./static/js/main.de56090b.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
